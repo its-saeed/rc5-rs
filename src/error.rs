022a61485e92cd03558e1fd7b790db7f20a48bb9
Saeed Dadkhah <saeed@zilliqa.com>
2024-06-10 05:18:20 +0330

    Cargo fmt

--- src/error.rs
+++ src/error.rs
@@ -2,27 +2,27 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
-  #[error("Invalid plaintext length")]
-  InvalidPlaintextLength,
+    #[error("Invalid plaintext length")]
+    InvalidPlaintextLength,
 
-  #[error("Invalid cipher text length")]
-  InvalidCipherTextLength,
+    #[error("Invalid cipher text length")]
+    InvalidCipherTextLength,
 
-  #[error("Invalid bytes. Can't convert to a word.")]
-  InvalidBytes,
+    #[error("Invalid bytes. Can't convert to a word.")]
+    InvalidBytes,
 
-  #[error("Provided key is not valid")]
-  InvalidKey,
+    #[error("Provided key is not valid")]
+    InvalidKey,
 
-  #[error("Key size is invalid")]
-  InvalidKeySize,
+    #[error("Key size is invalid")]
+    InvalidKeySize,
 
-  #[error("Word size is invalid")]
-  InvalidWordSize,
+    #[error("Word size is invalid")]
+    InvalidWordSize,
 
-  #[error("Invalid number of rounds")]
-  InvalidRoundsCount,
+    #[error("Invalid number of rounds")]
+    InvalidRoundsCount,
 
-  #[error(transparent)]
-  TryFromSliceError(#[from] std::array::TryFromSliceError),
+    #[error(transparent)]
+    TryFromSliceError(#[from] std::array::TryFromSliceError),
 }
